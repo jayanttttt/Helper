c250d6a425c3b48843c78038e871fc795c057439
Jayant <yash22350@gmail.com>
2022-03-06 15:12:05 +0530

    Footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,25 @@ import Group1 from "../images/Group1.png";
 import Group2 from "../images/Group2.png";
 import Group3 from "../images/Group3.png";
 import Group4 from "../images/Group4.png";
+import "./Footer.css";
 
 function Footer() {
   return (
     <>
-      <div>
-        <div>
-          <img src={Group1} alt="icon" />
-          <img src={Group2} alt="icon" />
-          <img src={Group3} alt="icon" />
-          <img src={Group4} alt="icon" />
+      <div className="footer_container_main">
+        <div className="footer_main">
+          <div>
+            <img src={Group1} alt="icon" />
+          </div>
+          <div>
+            <img src={Group2} alt="icon" />
+          </div>
+          <div>
+            <img src={Group3} alt="icon" />
+          </div>
+          <div>
+            <img src={Group4} alt="icon" />
+          </div>
         </div>
       </div>
     </>
