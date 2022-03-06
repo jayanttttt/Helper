bddfa10153d2d1243353b6b06709dfdb7aa5f476
Jayant <yash22350@gmail.com>
2022-03-06 17:46:48 +0530

    Half feedhome completed

--- src/App.js
+++ src/App.js
@@ -8,15 +8,49 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import { useState } from 'react';
+import Pic6 from "./images/Pic6.png";
+import Pic2 from "./images/Pic2.png";
+import Pic3 from "./images/Pic3.png";
+import Pic4 from "./images/Pic4.png";
+import Pic5 from "./images/Pic5.png";
 
 function App() {
+  const [feels] = useState([
+    {
+      img: Pic6,
+      title: "Happy",
+      id: 1,
+    },
+    {
+      img: Pic2,
+      title: "Confused",
+      id: 2,
+    },
+    {
+      img: Pic3,
+      title: "Worried",
+      id: 3,
+    },
+    {
+      img: Pic4,
+      title: "Depressed",
+      id: 4,
+    },
+    {
+      img: Pic5,
+      title: "Crying",
+      id: 5,
+    },
+  ])
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/createaccount" element={<CreateAccount />} />
-        <Route path="/feedhome" element={<FeedHome />} />
+        <Route path="/feedhome" element={<FeedHome feels={feels} />} />
       </Routes>
     </Router>
   );
--- src/components/FeedHome.jsx
+++ src/components/FeedHome.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import "./FeedHome.css";
 
-function FeedHome() {
+function FeedHome(props) {
+  const { feels } = props;
+  const [currentSelected, setCurrentSelected] = useState();
+  function handleSelect(feel) {
+    setCurrentSelected(feel);
+  }
+
   return (
     <>
       <Header />
+      <p className="feedhome_text_main">
+        Hi Rohan ! How are you feeling today ?
+      </p>
+      <div className="slider_container_main">
+        <div className="slider_container">
+          {feels.map((feel) => (
+            <div
+              key={feel.id}
+              className={
+                feel === currentSelected
+                  ? "feeling_container_active"
+                  : "feeling_container"
+              }
+              onClick={() => handleSelect(feel)}
+            >
+              <img src={feel.img} alt="img" />
+              <p className="slider_text_main">{feel.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className="feedhome_text_main">What makes you feel like this?</p>
+      <textarea placeholder="Share whatever that is disturbing you..." className="text_input" />
       <Footer />
     </>
   );
